97d1de82ce3f1b34f8c79140bc5b4a7a988b6c95
Brenton Carter <99493283+Inserian@users.noreply.github.com>
2026-08-10 14:10:49 -0800

    Export QSec 0.5 challenge types

--- qsec/__init__.py
+++ qsec/__init__.py
@@ -1,6 +1,7 @@
 from .engine import QSecPolicy, QuantumSecurityEngine
 from .model import Finding, FindingStatus, InspectionReport, SecuritySnapshot, Severity
 from .qsa_evidence import QSAEvidencePolicy, QSAEvidenceReceipt, QSAEvidenceRequest
+from .qsa_grover import QSAGroverPolicy, QSAGroverReceipt, QSAGroverRequest
 from .quantum_core import QuantumGate, QuantumProtocolError, QuantumRequest, QuantumWitness
 from .quantum_hopper import HopperMesh, MeshReceipt, QuantumReplayGuard
 from .threat_catalog import THREATS, ThreatDefinition, get_threat
@@ -14,6 +15,9 @@ __all__ = [
     "QSAEvidencePolicy",
     "QSAEvidenceReceipt",
     "QSAEvidenceRequest",
+    "QSAGroverPolicy",
+    "QSAGroverReceipt",
+    "QSAGroverRequest",
     "QSecPolicy",
     "QuantumGate",
     "QuantumProtocolError",
@@ -28,4 +32,4 @@ __all__ = [
     "get_threat",
 ]
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
