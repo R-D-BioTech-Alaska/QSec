ac4f9920e46dfe78cb0e77f974576cbc90540f04
Brenton Carter <99493283+Inserian@users.noreply.github.com>
2026-08-10 14:17:46 -0800

    Export QSA symmetry challenge types

--- qsec/__init__.py
+++ qsec/__init__.py
@@ -2,6 +2,7 @@ from .engine import QSecPolicy, QuantumSecurityEngine
 from .model import Finding, FindingStatus, InspectionReport, SecuritySnapshot, Severity
 from .qsa_evidence import QSAEvidencePolicy, QSAEvidenceReceipt, QSAEvidenceRequest
 from .qsa_grover import QSAGroverPolicy, QSAGroverReceipt, QSAGroverRequest
+from .qsa_symmetry import QSASymmetryPolicy, QSASymmetryReceipt, QSASymmetryRequest
 from .quantum_core import QuantumGate, QuantumProtocolError, QuantumRequest, QuantumWitness
 from .quantum_hopper import HopperMesh, MeshReceipt, QuantumReplayGuard
 from .threat_catalog import THREATS, ThreatDefinition, get_threat
@@ -18,6 +19,9 @@ __all__ = [
     "QSAGroverPolicy",
     "QSAGroverReceipt",
     "QSAGroverRequest",
+    "QSASymmetryPolicy",
+    "QSASymmetryReceipt",
+    "QSASymmetryRequest",
     "QSecPolicy",
     "QuantumGate",
     "QuantumProtocolError",
